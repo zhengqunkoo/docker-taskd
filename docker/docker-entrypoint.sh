bb553880cc7c88452b4ffa100ff784018954e6be
Armin Grodon <a.grodon@ryte.com>
2019-11-27 10:13:03 +0100

    Update alpine base image and gosu

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,15 +3,15 @@ set -e
 PKI=$TASKDDATA"/pki"
 
 if [ ! -d "$PKI" ]; then
-  mkdir -p $PKI
-  cp /usr/share/taskd/pki/generate* $PKI
-  cp /usr/share/taskd/pki/vars $PKI
+  mkdir -p "$PKI"
+  cp /usr/share/taskd/pki/generate* "$PKI"
+  cp /usr/share/taskd/pki/vars "$PKI"
   taskd init > /dev/null 2>&1
 fi
 
 # Generate self sign certificate if none exists
 if [ ! -f "$PKI/ca.cert.pem" ]; then
-  cd $PKI
+  cd "$PKI"
 
   if [ "$CERT_CN" ]; then
     sed -i "s/\(CN=\).*/\1'$CERT_CN'/" vars
@@ -30,20 +30,20 @@ if [ ! -f "$PKI/ca.cert.pem" ]; then
   fi
 
   ./generate
-  taskd config --force client.cert $PKI/client.cert.pem
-  taskd config --force client.key $PKI/client.key.pem
-  taskd config --force server.cert $PKI/server.cert.pem
-  taskd config --force server.key $PKI/server.key.pem
-  taskd config --force server.crl $PKI/server.crl.pem
-  taskd config --force ca.cert $PKI/ca.cert.pem
-  taskd config --force log $TASKDDATA/taskd.log
-  taskd config --force pid.file $TASKDDATA/taskd.pid
+  taskd config --force client.cert "$PKI/client.cert.pem"
+  taskd config --force client.key "$PKI/client.key.pem"
+  taskd config --force server.cert "$PKI/server.cert.pem"
+  taskd config --force server.key "$PKI/server.key.pem"
+  taskd config --force server.crl "$PKI/server.crl.pem"
+  taskd config --force ca.cert "$PKI/ca.cert.pem"
+  taskd config --force log "$TASKDDATA/taskd.log"
+  taskd config --force pid.file "$TASKDDATA/taskd.pid"
   taskd config --force server 0.0.0.0:53589
-  chown -R taskd:taskd $TASKDDATA
+  chown -R taskd:taskd "$TASKDDATA"
 fi
 
-if [ "$1" = 'taskd' -a "$(id -u)" = '0' ]; then
-  chown -R taskd:taskd $TASKDDATA
+if [ "$1" = 'taskd' ] && [ "$(id -u)" = '0' ]; then
+  chown -R taskd:taskd "$TASKDDATA"
   set -- gosu taskd "$@"
 fi
 
